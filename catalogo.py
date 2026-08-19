ca795de7af546e7487c5ca27133bd8d6bde918af
sofiaaacuellarrr <karencuellar@ufm.edu>
2026-08-19 13:29:50 -0600

    Funcionalidad de modificar elemento y prueba del programa

--- catalogo.py
+++ catalogo.py
@@ -42,5 +42,66 @@ def ver_marcas():
         print("Cruelty free:", informacion["cruelty_free"])
 
 
-agregar_marca()
-ver_marcas()
+def modificar_marca():
+    nombre = input("Nombre de la marca que desea modificar: ")
+
+    if nombre in catalogo_maquillaje:
+        print("1. Modificar fundadora")
+        print("2. Modificar pais")
+        print("3. Modificar cruelty free")
+
+        opcion = input("Seleccione un atributo: ")
+
+        if opcion == "1":
+            nueva_fundadora = input("Nueva fundadora o fundador: ")
+            catalogo_maquillaje[nombre]["fundadora"] = nueva_fundadora
+            print("Fundadora modificada correctamente")
+
+        elif opcion == "2":
+            nuevo_pais = input("Nuevo pais: ")
+            catalogo_maquillaje[nombre]["pais"] = nuevo_pais
+            print("Pais modificado correctamente")
+
+        elif opcion == "3":
+            respuesta = input("Es cruelty free? Escriba si o no: ")
+
+            if respuesta == "si":
+                catalogo_maquillaje[nombre]["cruelty_free"] = True
+            else:
+                catalogo_maquillaje[nombre]["cruelty_free"] = False
+
+            print("Cruelty free modificado correctamente")
+
+        else:
+            print("Opcion incorrecta")
+
+    else:
+        print("La marca no existe")
+
+
+opcion = ""
+
+while opcion != "4":
+    print("\nMENU")
+    print("1. Ver todas las marcas")
+    print("2. Agregar una marca")
+    print("3. Modificar una marca")
+    print("4. Salir")
+
+    opcion = input("Seleccione una opcion: ")
+
+    if opcion == "1":
+        ver_marcas()
+
+    elif opcion == "2":
+        agregar_marca()
+
+    elif opcion == "3":
+        modificar_marca()
+
+    elif opcion == "4":
+        print("Programa finalizado")
+
+    else:
+        print("Opcion incorrecta")
+        
